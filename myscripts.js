edd8aa6e43b1195e3a81bb6ac0cf278df853140f
Jespermagician <jesper@herlings.de>
2024-12-31 17:54:51 +0100

    Add more css, add button event

--- myscripts.js
+++ myscripts.js
@@ -1,3 +1,18 @@
+document.addEventListener('DOMContentLoaded', function() 
+{
+    const btns = document.querySelectorAll('#numpad .btn'); 
+  
+    btns.forEach(btn => {
+      // Fügen Sie hier Event-Listener für jeden Button hinzu
+      btn.addEventListener('click', function() 
+      {
+        // Logik für den Button-Klick hier
+        console.log(btn.textContent); // Zeigt den Textinhalt des geklickten Buttons an
+      });
+    });
+  });
+
+
 function main()
 {
     
