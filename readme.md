4d136c3142952569c2c162c64bea0d8d9f2155b5
Jespermagician <jesper@herlings.de>
2025-06-01 21:15:53 +0200

    dunno what i did here

--- readme.md
+++ readme.md
@@ -2,4 +2,17 @@
 
 A small Test Projekt. The goal is to make it more complex step for step.
 
-nvm i switch to typescript 
+nvm i switch to typescript.
+
+</ hr>
+
+## Setup
+
+`npm init -y`
+`npm install typescript --save-dev`
+
+<!-- `npx tsc --init` -->
+
+## Apply Changes
+
+`npx tsc`
--- script/script.ts
+++ script/script.ts
@@ -1,23 +1,3 @@
-// document.addEventListener('DOMContentLoaded', function() 
-// {
-//     const btns = document.querySelectorAll('#numpad .btn'); 
-  
-//     btns.forEach(btn => {
-//       // Fügen Sie hier Event-Listener für jeden Button hinzu
-//       btn.addEventListener('click', function() 
-//       {
-//         // Logik für den Button-Klick hier
-//         console.log(btn.textContent); // Zeigt den Textinhalt des geklickten Buttons an
-//       });
-//     });
-//   });
-
-
-
-function main()
-{
-    
-}
 class Calc {
   constructor(
     private result: number = 0,
